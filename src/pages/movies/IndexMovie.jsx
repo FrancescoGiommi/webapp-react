0d0256fb3705d2cb173c46015efe9de948d41fbc
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-13 17:02:17 +0100

    Add fetch movies

--- src/pages/movies/IndexMovie.jsx
+++ src/pages/movies/IndexMovie.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function IndexMovie() {
   const movies = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:3000/api/movies";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   return (
     <>
