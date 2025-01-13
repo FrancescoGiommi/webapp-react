6ce7e639e37d5894ccbc37814c0faa8ffb29388b
FrancescoGiommi <francescogiommi97@icloud.com>
2025-01-13 15:59:53 +0100

    Add HomePage, AboutPage and DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -30,13 +30,18 @@
 
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import DefaultLayout from "./layouts/DefaultLayout";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route element={<DefaultLayout />} path="/">
+            <Route index element={<HomePage />}></Route>
+            <Route path="about" element={<AboutPage />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout() {
+  return (
+    <>
+      <Header></Header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer></footer>
+    </>
+  );
+}
